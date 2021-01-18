bb8817ac41e6975f7ed2109de4fab9aed4f18e9f
bouchentouf hatim <hatimbo28@gmail.com>
2021-01-18 21:32:59 +0100

    frontend finished

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 import Modal from "../../shared/components/UIElements/Modal";
 import Card from "../../shared/components/UIElements/Card";
 import "./PlaceItem.css";
 import Map from "../../shared/components/UIElements/Map";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../shared/context/auth-context";
 const PlaceItem = (props) => {
+  const auth = useContext(AuthContext);
   const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
   const openMapHandler = () => setShowMap(true);
@@ -59,11 +61,14 @@ const PlaceItem = (props) => {
           </div>
           <div className="place-item__actions">
             <button onClick={openMapHandler}>VIEW ON MAP</button>
-            <Link to={`/places/${props.id}`}>
-              <button>EDIT</button>
-            </Link>
-
-            <button onClick={showDeleteWarningHandler}>DELETE</button>
+            {auth.isLoggedIn && (
+              <Link to={`/places/${props.id}`}>
+                <button>EDIT</button>
+              </Link>
+            )}
+            {auth.isLoggedIn && (
+              <button onClick={showDeleteWarningHandler}>DELETE</button>
+            )}
           </div>
         </Card>
       </div>
